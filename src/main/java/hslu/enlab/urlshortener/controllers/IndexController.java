8a5dd35d7afe4cf265ffa65b7992083505552144
boasmeier <62314872+boasmeier@users.noreply.github.com>
2022-04-29 20:05:12 +0200

    feat: serve index.html with thymeleaf

--- src/main/java/hslu/enlab/urlshortener/controllers/IndexController.java
+++ src/main/java/hslu/enlab/urlshortener/controllers/IndexController.java
@@ -1,14 +1,14 @@
 package hslu.enlab.urlshortener.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
 
     @GetMapping("/")
     public String index() {
-        return "Greetings from URL-Shortener! Deploy job works! :D";
+        return "index";
     }
 
 }
