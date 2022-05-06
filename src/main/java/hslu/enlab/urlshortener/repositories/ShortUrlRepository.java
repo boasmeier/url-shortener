79d978bbca54583aca1612d980172a401906c1c7
boasmeier <62314872+boasmeier@users.noreply.github.com>
2022-05-06 20:10:00 +0200

    fix: delete endpoint by adding @Transactional

--- src/main/java/hslu/enlab/urlshortener/repositories/ShortUrlRepository.java
+++ src/main/java/hslu/enlab/urlshortener/repositories/ShortUrlRepository.java
@@ -4,11 +4,13 @@ import hslu.enlab.urlshortener.entities.ShortUrl;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.UUID;
 
 @Repository
 public interface ShortUrlRepository extends JpaRepository<ShortUrl, UUID> {
 
+    @Transactional
     void deleteShortUrlById(UUID id);
 
     boolean existsUrlEntityByShortUrl(String shortUrl);
