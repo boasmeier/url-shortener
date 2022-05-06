d82ac13585562c2cc3d79b1d9fe264979557e847
boasmeier <62314872+boasmeier@users.noreply.github.com>
2022-05-06 19:14:58 +0200

    test: improve ShortUrlServiceTest coverage to 100%

--- src/test/java/hslu/enlab/urlshortener/services/ShortUrlServiceTest.java
+++ src/test/java/hslu/enlab/urlshortener/services/ShortUrlServiceTest.java
@@ -4,6 +4,7 @@ import hslu.enlab.urlshortener.entities.ShortUrl;
 import hslu.enlab.urlshortener.repositories.ShortUrlRepository;
 import hslu.enlab.urlshortener.services.ShortUrlService;
 import hslu.enlab.urlshortener.services.StatisticService;
+import org.assertj.core.api.AssertionsForClassTypes;
 import org.assertj.core.util.VisibleForTesting;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,9 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.argThat;
 
 @ExtendWith(MockitoExtension.class)
 class ShortUrlServiceTest {
@@ -31,6 +35,20 @@ class ShortUrlServiceTest {
     void shouldCreateShortUrl() {
         // arrange
         String url = "https://google.ch";
+
+        // act
+        testee.create(url);
+
+        // assert
+        Mockito.verify(shortUrlRepository).save(Mockito.any(ShortUrl.class));
+    }
+
+    @Test
+    void shouldCreateUniqueShortUrl() {
+        // arrange
+        String url = "www.google.ch";
+
+        Mockito.when(shortUrlRepository.existsUrlEntityByShortUrl(Mockito.any(String.class))).thenReturn(true).thenReturn(false);
 
         // act
         testee.create(url);
@@ -79,6 +97,54 @@ class ShortUrlServiceTest {
 
         // assert
         Mockito.verify(shortUrlRepository).deleteShortUrlById(id);
+    }
+
+    @Test
+    void shouldReturnShortUrlIfShortedUrlExist() {
+        // arrange
+        String shortedUrl = "TEST";
+
+        ShortUrl expectedShortUrl = new ShortUrl();
+        expectedShortUrl.setId(UUID.randomUUID());
+        expectedShortUrl.setUrl("https://existingurl.com");
+
+        Mockito.when(shortUrlRepository.findUrlEntityByShortUrl(shortedUrl)).thenReturn(expectedShortUrl);
+
+        // act
+        testee.findUrl(shortedUrl);
+
+        // assert
+        Mockito.verify(shortUrlRepository).findUrlEntityByShortUrl(shortedUrl);
+    }
+
+    @Test
+    void shouldThrowIfShortedUrlDontExist() {
+        // arrange
+        String shortedUrl = "TEST";
+
+        // act
+        RuntimeException actual = Assertions.assertThrows(RuntimeException.class, () -> testee.findUrl(shortedUrl));
+
+        // assert
+        assertThat(actual.getMessage()).isEqualTo("Link not found");
+    }
+
+    @Test
+    void shouldUpdateUUID() {
+        // arrange
+        UUID newId = UUID.randomUUID();
+        ShortUrl shortUrl = new ShortUrl();
+        shortUrl.setId(UUID.randomUUID());
+        shortUrl.setUrl("https://existingurl.com");
+
+        // act
+        testee.put(shortUrl, newId);
+
+        // assert
+        Mockito.verify(shortUrlRepository).save(argThat(s -> {
+            AssertionsForClassTypes.assertThat(s.getId()).isEqualTo(newId);
+            return true;
+        }));
     }
 
 }
