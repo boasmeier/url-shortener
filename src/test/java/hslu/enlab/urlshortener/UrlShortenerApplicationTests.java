45cda967123b58658a726e47ce7993f12ea24f75
boasmeier <62314872+boasmeier@users.noreply.github.com>
2022-05-06 18:17:35 +0200

    refactor: add assert to UrlShortenerApplicationTests

--- src/test/java/hslu/enlab/urlshortener/UrlShortenerApplicationTests.java
+++ src/test/java/hslu/enlab/urlshortener/UrlShortenerApplicationTests.java
@@ -1,13 +1,24 @@
 package hslu.enlab.urlshortener;
 
+import hslu.enlab.urlshortener.controllers.IndexController;
+import hslu.enlab.urlshortener.controllers.RedirectController;
+import hslu.enlab.urlshortener.controllers.ShortUrlController;
+import hslu.enlab.urlshortener.controllers.StatisticController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class UrlShortenerApplicationTests {
 
+	@Autowired
+	private IndexController indexController;
+
 	@Test
 	void contextLoads() {
+		assertThat(indexController).isNotNull();
 	}
 
 }
